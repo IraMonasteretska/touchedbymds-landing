5883b7f6c122cd83fbf2fd5d402c757abad7d503
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-05-31 09:31:31 +0300

    add new project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// plyr player
+const player = new Plyr('#player');
+$(document).ready(function () {
+
+// scroll top
+$('.back-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 0);
+    return false;
+});
+
+});
+
+
+  
